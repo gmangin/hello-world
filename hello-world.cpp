d73899c642af63eb14742777d851ef9d7f68eedc
Gaelle MANGIN <gmangin@62-210-75-32.online.net>
2015-04-29 10:39:55 +0200

    gmangin

---
+++ hello-world.cpp
@@ -0,0 +1,19 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                                            //
+//   hello-world.cpp                                                          //
+//                                                                            //
+//   By: gmangin <gaelle.mangin@hotmail.fr>                                   //
+//                                                                            //
+//   Created: 2015/04/29 10:16:06 by gmangin                                  //
+//   Updated: 2015/04/29 10:17:14 by gmangin                                  //
+//                                                                            //
+// ************************************************************************** //
+
+#include	<iostream>
+
+int		main(void)
+{
+	std::cout << "hello world" << std::endl;
+	return (0);
+}
